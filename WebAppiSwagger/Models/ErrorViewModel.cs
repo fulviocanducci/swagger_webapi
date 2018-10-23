29e1eb086cd65c627d6388904e593ebb21e37233
Fulvio <fulviocanducci@hotmail.com>
2018-10-23 19:53:56 -0300

    Add project files.

---
+++ WebAppiSwagger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppiSwagger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
